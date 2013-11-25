dd95677c2220919702a01c110c57816815e78b56
Erdong Feng <edfeng@thoughtworks.com>
2013-11-25 20:10:58 +0800

    enable pagination forward and back.

--- js/app/index/controller.js
+++ js/app/index/controller.js
@@ -2,11 +2,38 @@ MyApp.controller("eventsController",
     ["$scope", "$compile",  "eventsService",
     function($scope, $compile, eventsService) {
       $scope.pageSize = 10;
-      $scope.currentPage = 1;
+      $scope.currentPage = 0;
       $scope.init = function() {
         eventsService.events().then(function(response) {
           $scope.events = response.data;
+          $scope.pageNumbers = Math.ceil($scope.events.length / $scope.pageSize);
         });
+      };
+
+      $scope.first = function()  {
+        $scope.currentPage = 0;
+      };
+
+      $scope.next = function() {
+        if ($scope.lastPage()) return;
+        $scope.currentPage += 1;
+      };
+
+      $scope.previous = function() {
+        if ($scope.firstPage()) return;
+        $scope.currentPage -= 1;
+      };
+
+      $scope.last = function() {
+        $scope.currentPage = $scope.pageNumbers - 1;
+      };
+
+      $scope.firstPage = function() {
+        return $scope.currentPage == 0;
+      };
+
+      $scope.lastPage = function() {
+        return $scope.currentPage == $scope.pageNumbers - 1;
       };
 
       $scope.remove = function(index) {
